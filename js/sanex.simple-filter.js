133ab882da5e3f6228ae09884e032b81eaed60f3
sanex3339 <sanex3339@yandex.ru>
2016-03-19 18:54:39 +0300

    refactoring

--- js/sanex.simple-filter.js
+++ js/sanex.simple-filter.js
@@ -1,117 +1,146 @@
 (function() {
     var simpleFilter = function () {
-        var dataWrapper = $('.fltr-data-wrapper');
-        var filter = {};
-        var oldGetParams = [];
+        var $dataWrapper = $('.fltr-data-wrapper'),
+            filter = {},
+            oldGetParams = [],
+            FILTER_PARAMETER_NAME = 'filter';
 
         var filterObject = {
             init: function () {
-                if (!this.historyApiCheck())
+                if (!this.historyApiCheck()) {
                     SimpleFilterAjax = false;
-                this.events();
+                }
+
+                this.bindEvents();
                 this.getCheckboxState();
-                if (!SimpleFilterAjax)
+
+                if (!SimpleFilterAjax) {
                     this.setFilterUrls();
-            },
-            historyApiCheck: function () {
-                return !!(window.history && history.pushState);
-            },
-            events: function () {
-                var self = this;
-                $(document).ready(function () {
-                    $('.fltr-wrapper .fltr-check').click(function () {
-                        oldGetParams = window.location.search.substring(1);
-                        if (SimpleFilterAjax)
-                            self.setQueryUrl($(this));
-                        self.setFilterData();
-                    });
-                });
-            },
-            getCheckboxState: function () {
-                var urlParams = this.getQueryParameters(window.location.search.substring(1));
-                delete urlParams['filter'];
-                for (params in urlParams) {
-                    var category = params.split('[', 1),
-                        properties = urlParams[params],
-                        element = $('.fltr-wrapper .fltr-cat[data-property="' + category + '"]');
-                    element.find('.fltr-check[value="' + properties + '"]').addClass('active');
                 }
             },
-            getQueryParameters: function (href) {
-                var urlParams;
-                (window.onpopstate = function () {
-                    var match,
-                        pl = /\+/g,
-                        search = /([^&=]+)=?([^&]*)/g,
-                        decode = function (s) {
-                            return decodeURIComponent(s.replace(pl, " "));
-                        },
-                        query = decodeURIComponent(href);
-                    urlParams = {};
-                    while (match = search.exec(query)) {
-                        if (urlParams[decode(match[1])]) {
-                            urlParams[decode(match[1])] += ',' + decode(match[2]);
-                        } else {
-                            urlParams[decode(match[1])] = decode(match[2]);
-                        }
+
+            historyApiCheck: function () {
+                return window.history && history.pushState;
+            },
+
+            bindEvents: function () {
+                var self = this;
+
+                $('.fltr-wrapper .fltr-check').click(function () {
+                    oldGetParams = window.location.search.substring(1);
+
+                    if (SimpleFilterAjax) {
+                        self.setQueryUrl($(this));
                     }
-                })();
+
+                    self.setFilterData();
+                });
+            },
+
+            getCheckboxState: function () {
+                var urlParams = this.getQueryParameters(window.location.search.substring(1));
+
+                for (var params in urlParams) {
+                    var category = params.split('[', 1),
+                        properties = urlParams[params],
+                        $element = $('.fltr-wrapper .fltr-cat[data-property="' + category + '"]');
+
+                    if (params === FILTER_PARAMETER_NAME) {
+                        continue;
+                    }
+
+                    $element.find('.fltr-check[value="' + properties + '"]').addClass('active');
+                }
+            },
+
+            getQueryParameters: function (href) {
+                var urlParams = {},
+                    match,
+                    search = /([^&=]+)=?([^&]*)/g,
+                    decode = function (s) {
+                        return decodeURIComponent(s.replace(/\+/g, " "));
+                    },
+                    query = decodeURIComponent(href);
+
+                while (match = search.exec(query)) {
+                    if (urlParams[decode(match[1])]) {
+                        urlParams[decode(match[1])] += ',' + decode(match[2]);
+                    } else {
+                        urlParams[decode(match[1])] = decode(match[2]);
+                    }
+                }
+
                 return urlParams;
             },
+
             sendFilter: function (filterJsonFormat) {
-                var self = this;
                 $.ajax({
                     url: SimpleFilterAjaxUrl + '?' + oldGetParams,
                     type: 'POST',
                     data: {_csrf: yii.getCsrfToken(), filter: filterJsonFormat},
                     dataType: 'html',
                     success: function (data) {
-                        dataWrapper.children().remove();
-                        dataWrapper.html(data);
+                        $dataWrapper.children().remove();
+                        $dataWrapper.html(data);
                     }
                 });
             },
             setFilterData: function () {
                 var counter = 0,
+                    filterJsonFormat,
                     prevCategory;
+
                 $('.fltr-wrapper .fltr-cat').each(function () {
                     var array = [],
-                        category = $(this).data('property');
-                    if (category != prevCategory)
+                        category = $(this).data('property'),
+                        property;
+
+                    if (category != prevCategory) {
                         counter = 0;
+                    }
+
                     $(this).find('.fltr-check.active').each(function (index) {
                         array[index] = $(this).attr('value');
                     });
-                    var property = array.join();
+
+                    property = array.join();
+
                     if (property.length > 0) {
                         filter[category + '[' + counter + ']'] = {properties: property};
                     } else {
                         delete filter[category + '[' + counter + ']'];
                     }
+
                     counter++;
                     prevCategory = category;
                 });
-                var filterJsonFormat = JSON.stringify(filter);
+
+                filterJsonFormat = JSON.stringify(filter);
                 this.sendFilter(filterJsonFormat);
             },
+
             setFilterUrls: function () {
                 $('.fltr-wrapper .fltr-check').each(function (){
                     var elem = $(this),
                         category = elem.parent().data('property'),
                         filterGetParameter,
                         getQuery = window.location.search.substring(1);
+
                     if (!elem.hasClass('active')) {
                         filterGetParameter = $.query.set('filter', '1').SET(category + '[]', elem.attr('value')).toString();
                     } else {
                         filterGetParameter = $.query.remove(category, elem.attr('value'));
                     }
+
                     $(this).attr('href', filterGetParameter);
                 });
             },
             setQueryUrl: function (elem) {
                 var category = elem.parent().data('property'),
                     url;
+
                 $.query.parseNew(location.search, location.hash.split("?").length > 1 ? location.hash.split("?")[1] : "");
+
                 if (!elem.hasClass('active')) {
                     url = $.query.SET('filter', '1').SET(category + '[]', elem.attr('value')).toString();
                     elem.addClass('active');
@@ -119,11 +148,14 @@
                     url = $.query.REMOVE(category, elem.attr('value'));
                     elem.removeClass('active');
                 }
+
                 window.history.pushState('', '', url);
                 oldGetParams = window.location.search.substring(1);
             }
         };
+
         filterObject.init();
     };
+
     return simpleFilter();
 })();
